252648cc2986899afeedb757b75e9a082dacabdb
caiohenrique-developer <caiorussellrox@msn.com>
2021-05-11 10:26:58 -0300

    :hammer: [ CHORE ]: Add styled-components lib

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/globals.css';
+import GlobalsStyle from '@styles/globals';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+
+      <GlobalsStyle />
+    </>
+  );
 }
 
 export default MyApp;
---
+++ src/styles/globals.ts
@@ -0,0 +1,48 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  /* Root variables */
+  :root { --background:#121212; }
+
+  * {
+    border: 0;
+    margin: 0;
+    padding: 0;
+    outline: none;
+    box-sizing: border-box;
+
+    font: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif 1rem;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  html {
+    @media(max-width: 1080px) { font-size: 93.75%; }
+
+    @media(max-width: 720px) { font-size: 87.5%; }
+
+    body, input, textarea, select, button { font-weight: 400; }
+
+    body {
+      background: var(--background);
+      color: #ccc;
+
+      button { cursor: pointer; }
+
+      [disabled] {
+        opacity: 0.5;
+        cursor: not-allowed;
+      }
+
+      h1, h2, h3, h4, h5, h6, strong { font-weight: 600; }
+
+      ol, ul { list-style: none; }
+
+      a {
+        color: inherit;
+        text-decoration: none;
+      }
+    }
+  }
+`;
