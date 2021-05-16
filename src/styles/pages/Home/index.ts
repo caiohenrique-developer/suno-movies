88f3468cccd6ddfdff84d12123af560d401a7f46
caiohenrique-developer <caiorussellrox@msn.com>
2021-05-16 02:39:17 -0300

    :sparkles: [ FEAT ]: Home page aside responsive done

--- src/styles/pages/Home/index.ts
+++ src/styles/pages/Home/index.ts
@@ -1,34 +1,42 @@
 import styled from 'styled-components';
 
 export const Container = styled.main`
-  margin-top: min(114px, 7.125rem);
+  position: relative;
+
+  margin-top: max(9vw, 7.125rem);
 
   section {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+
     max-width: 1200px;
-    margin: 0 auto;
+    margin: auto;
 
     aside {
       position: fixed;
-      top: max(50%, 30%);
+      top: min(50%, 33vw);
 
-      transform: translateY(min(-50%, -30%));
-      padding: 20px;
-      margin: 20px;
+      transform: translateY(max(-50%, -1vw));
+      padding: max(1.25rem, 1.57vw);
+      margin: auto max(1.25rem, 1.57vw);
       color: var(--white_EAE);
       -webkit-backdrop-filter: blur(6px);
       backdrop-filter: blur(6px);
-      background: rgba(0, 0, 0, 0.3);
+      background: rgba(0, 0, 0, 0.2);
       box-shadow: 5px 5px 5px var(--black_181);
       border-radius: 8px;
       border: 1px solid rgba(255, 255, 255, 0.15);
 
       h1 {
-        font-size: min(44px, 2.75rem);
+        font-size: min(3.5vw, 2.75rem);
       }
 
       p {
-        font-size: min(1rem, 1.625rem);
-        margin: 10px 0 20px;
+        font-size: min(1.7vw, 1.625rem);
+        margin: 0.625rem 0 1.25rem;
       }
     }
   }
