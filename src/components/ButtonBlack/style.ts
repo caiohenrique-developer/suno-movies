af1e002f7298f7e0db2480a85303d236d7368a74
caiohenrique-developer <caiorussellrox@msn.com>
2021-05-16 14:37:34 -0300

    :art: [ STYLE ]: Correct button black responsive

--- src/components/ButtonBlack/style.ts
+++ src/components/ButtonBlack/style.ts
@@ -3,16 +3,17 @@ import styled from 'styled-components';
 export const Container = styled.a`
   display: flex;
   align-items: center;
+  justify-content: center;
 
-  max-width: 11.6vw;
+  max-width: min(9.375rem, 11.6vw);
   min-width: 50px;
   width: 100%;
-  padding: 0.8rem 1.25rem;
+  padding: min(0.8rem, 2vw) min(1.25rem, 2vw);
   cursor: pointer;
   background: linear-gradient(0deg, var(--gray_2E2) 0%, var(--black_212) 100%);
   border-radius: 8px;
   border: 2px solid var(--black_2E2);
-  font-size: 1rem;
+  font-size: min(1rem, 1.3vw);
   text-transform: uppercase;
   transition: 0.8s;
 
@@ -21,8 +22,9 @@ export const Container = styled.a`
   }
 
   i {
-    width: 0.6rem;
-    margin-left: 0.6rem;
+    width: min(0.6rem, 1vw);
+    min-width: 4px;
+    margin-left: min(0.6rem, 1vw);
 
     svg {
       display: block;
