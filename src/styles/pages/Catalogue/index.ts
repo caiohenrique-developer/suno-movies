09a482da2de758285c2f31e25e2377dcf071d356
caiohenrique-developer <caiorussellrox@msn.com>
2021-05-18 16:04:15 -0300

    :art: [ STYLE ]: Correct header height to buth pages

--- src/styles/pages/Catalogue/index.ts
+++ src/styles/pages/Catalogue/index.ts
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 
 export const Container = styled.main`
-  margin-top: min(9vw, 7.125rem);
+  margin-top: min(15vw, 8.125rem);
+  padding: 20px min(1.25rem, 1.57vw);
 
   color: pink;
-  font-size: 5rem;
+  font-size: 3rem;
 `;
