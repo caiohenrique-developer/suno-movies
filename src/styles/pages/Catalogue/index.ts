752e8a32696d3b2615a36db2a9ad6c7e954dd49c
caiohenrique-developer <caiorussellrox@msn.com>
2021-05-18 16:10:06 -0300

    :art: [ STYLE ]: Center main section for catalogue page

--- src/styles/pages/Catalogue/index.ts
+++ src/styles/pages/Catalogue/index.ts
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 
 export const Container = styled.main`
-  margin-top: min(15vw, 8.125rem);
-  padding: 20px min(1.25rem, 1.57vw);
+  max-width: 1200px;
+  margin: min(15vw, 8.125rem) auto 0;
+  padding: 1.25rem min(1.25rem, 1.57vw);
 
   color: pink;
   font-size: 3rem;
