d6735f3b43e85123b7324cb7b4016f7f72f62076
caiohenrique-developer <caiorussellrox@msn.com>
2021-05-24 20:47:46 -0300

    :art: [ STYLE ]: Add border to each button types

--- src/styles/components/CTAButton/index.ts
+++ src/styles/components/CTAButton/index.ts
@@ -8,11 +8,13 @@ export const BaseBtnStyles = css`
   min-width: 50px;
   padding: min(0.8rem, 2vw) min(1.25rem, 2vw);
   cursor: pointer;
+  border: 2px solid;
   border-radius: 8px;
   font-size: min(1rem, 1.3vw);
   transition: 0.8s;
 
   &.btn-black {
+    border-color: var(--gray_2E2);
     background: linear-gradient(
       0deg,
       var(--gray_2E2) 0%,
@@ -21,6 +23,7 @@ export const BaseBtnStyles = css`
   }
 
   &.btn-pink {
+    border-color: var(--pink_FE3);
     background: linear-gradient(0deg, var(--pink_FE3) 0%, var(--pink_FF0) 100%);
   }
 
