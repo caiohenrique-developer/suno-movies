8ad7b8a95c9230353e4b6e85dc244c62deff02d9
caiohenrique-developer <caiorussellrox@msn.com>
2021-05-24 16:14:51 -0300

    :chart_with_upwards_trend: [ WIP ]: Catalogue list layout without hover effect

--- src/components/CardMovie/style.ts
+++ src/components/CardMovie/style.ts
@@ -4,44 +4,45 @@ export const Container = styled.div`
   display: flex;
 
   & + div {
-    margin-top: 38px;
+    margin-top: min(2.375rem, 3vw);
   }
 
   > div {
     &:first-of-type {
-      width: 158px;
+      max-width: min(9.875rem, 12.7vw) !important;
+      min-width: min(5.5rem, 7.5vw);
+      width: 100%;
     }
 
     &:last-of-type {
-      max-width: 80%;
-      margin-left: 28px;
+      margin-left: min(1.75rem, 2.8vw);
 
       h4 {
-        font-size: 21.15px;
+        font-size: min(1.4rem, 1.8vw);
         color: var(--white_EAE);
       }
 
       h3 {
-        font-size: 15.39px;
+        font-size: min(1rem, 1.4vw);
         font-weight: 300;
         color: var(--pink_FE3);
       }
 
       span {
         display: block;
-        margin: 12px 0 20px;
-        font-size: 15.39px;
+        margin: min(0.8rem, 1.1vw) 0 min(1.25rem, 1.8vw);
+        font-size: min(1rem, 1.4vw);
         color: var(--white_EAE);
 
         svg {
-          font-size: 1rem;
-          margin-right: 10px;
+          font-size: min(1.1rem, 1.6vw);
+          margin-right: min(0.6rem, 1vw);
           color: var(--pink_FE3);
         }
       }
 
       p {
-        font-size: 12.5px;
+        font-size: min(0.8rem, 1.1vw);
         font-weight: 300;
         color: var(--gray_9F9);
       }
