9bc2461aab54d376fdb4546c2ca2fdd170b0117e
caiohenrique-developer <caiorussellrox@msn.com>
2021-06-04 21:31:48 -0300

    :rotating_light: [ TEST ]: Api request test with the movie-discover file

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -23,48 +23,6 @@ const genres = 'genre/movie/list';
 const ptBR = 'pt-BR';
 const apiKey = process.env.API_KEY;
 
-export default async function tst(req: NextApiRequest, res: NextApiResponse) {
-  const { data: result } = await api.get(movieDiscover, {
-    params: {
-      page: 8,
-      api_key: apiKey,
-      language: ptBR,
-      include_adult: false,
-    },
-  });
-
-  const tst = result['results'].map((movie) => {
-    const dataMounted = {
-      id: movie['id'],
-      title: movie['title'],
-      description: movie['overview'],
-      genreIDs: movie['genre_ids'],
-      rating: movie['vote_average'],
-    };
-
-    if (
-      movie['poster_path'] == null ||
-      movie['poster_path'] == undefined ||
-      movie['poster_path'] == ''
-    )
-      return {
-        ...dataMounted,
-        poster: '/assets/poster-placeholder.png',
-      };
-    else
-      return {
-        ...dataMounted,
-        poster: `https://image.tmdb.org/t/p/original${movie['poster_path']}`,
-      };
-  });
-
-  return res.json({
-    ok: true,
-    api_req: 'Requisição pelo arquivo index.ts!',
-    tst,
-  });
-}
-
 export const fetchMovieDiscover = async (): Promise<
   FetchMovieDiscoverProps[]
 > => {
