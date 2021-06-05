efced27839eede713f9e619b8a6f947a3c442fbc
caiohenrique-developer <caiorussellrox@msn.com>
2021-06-04 21:48:15 -0300

    :rotating_light: [ TEST ]: Movie discover api just returning test datas

--- src/pages/api/movie-discover.ts
+++ src/pages/api/movie-discover.ts
@@ -9,43 +9,8 @@ const ptBR = 'pt-BR';
 const apiKey = process.env.API_KEY;
 
 export default async function tst(req: NextApiRequest, res: NextApiResponse) {
-  const { data: result } = await api.get(movieDiscover, {
-    params: {
-      page: 8,
-      api_key: apiKey,
-      language: ptBR,
-      include_adult: false,
-    },
-  });
-
-  const tst = result['results'].map((movie) => {
-    const dataMounted = {
-      id: movie['id'],
-      title: movie['title'],
-      description: movie['overview'],
-      genreIDs: movie['genre_ids'],
-      rating: movie['vote_average'],
-    };
-
-    if (
-      movie['poster_path'] == null ||
-      movie['poster_path'] == undefined ||
-      movie['poster_path'] == ''
-    )
-      return {
-        ...dataMounted,
-        poster: '/assets/poster-placeholder.png',
-      };
-    else
-      return {
-        ...dataMounted,
-        poster: `https://image.tmdb.org/t/p/original${movie['poster_path']}`,
-      };
-  });
-
-  return res.json({
+  return res.status(200).json({
     ok: true,
     api_req: 'Requisição pelo arquivo index.ts!',
-    tst,
   });
 }
