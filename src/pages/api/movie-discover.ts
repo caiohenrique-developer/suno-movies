441eadca6e685f09f862eada6835f005ce4393ae
caiohenrique-developer <caiorussellrox@msn.com>
2021-06-04 22:16:25 -0300

    :sparkles: [ FEAT ]: Create the correct fetchMovieDiscover api end point file

--- src/pages/api/movie-discover.ts
+++ src/pages/api/movie-discover.ts
@@ -1,5 +1,5 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
 import { api } from '.';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 // route
 const movieDiscover = 'discover/movie';
@@ -8,9 +8,47 @@ const movieDiscover = 'discover/movie';
 const ptBR = 'pt-BR';
 const apiKey = process.env.API_KEY;
 
-export default async function tst(req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).json({
-    ok: true,
-    api_req: 'Requisição pelo arquivo index.ts!',
-  });
+export default async function fetchMovieDiscover(
+  req: NextApiRequest,
+  res: NextApiResponse,
+): Promise<void> {
+  try {
+    const { data: result } = await api.get(movieDiscover, {
+      params: {
+        page: 8,
+        api_key: apiKey,
+        language: ptBR,
+        include_adult: false,
+      },
+    });
+
+    const resultMounted = result['results'].map((movie) => {
+      const dataMounted = {
+        id: movie['id'],
+        title: movie['title'],
+        description: movie['overview'],
+        genreIDs: movie['genre_ids'],
+        rating: movie['vote_average'],
+      };
+
+      if (
+        movie['poster_path'] == null ||
+        movie['poster_path'] == undefined ||
+        movie['poster_path'] == ''
+      )
+        return {
+          ...dataMounted,
+          poster: '/assets/poster-placeholder.png',
+        };
+      else
+        return {
+          ...dataMounted,
+          poster: `https://image.tmdb.org/t/p/original${movie['poster_path']}`,
+        };
+    });
+
+    return res.status(200).json(resultMounted);
+  } catch (err) {
+    console.error(err);
+  }
 }
