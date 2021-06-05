b9cc89b61c8fa5e805397a925608e546900474b3
caiohenrique-developer <caiorussellrox@msn.com>
2021-06-04 21:03:01 -0300

    :rotating_light: [ TEST ]: Push the tst function to test api request on vercel

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,3 +1,4 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
 
 import {
@@ -7,7 +8,7 @@ import {
   FetchSearchMovieProps,
 } from '@utils/types/api';
 
-const api = axios.create({
+export const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
 });
 
@@ -15,12 +16,54 @@ const api = axios.create({
 const movieDiscover = 'discover/movie';
 const searchMovie = 'search/movie';
 const topRated = 'movie/top_rated';
-// https://api.themoviedb.org/3/movie/top_rated?api_key=4187889f19ab87479fdbfad7e23fa800
+// https://api.themoviedb.org/3/movie/top_rated?api_key=key
 const genres = 'genre/movie/list';
 
 // route params
 const ptBR = 'pt-BR';
 const apiKey = process.env.API_KEY;
+
+export default async function tst(req: NextApiRequest, res: NextApiResponse) {
+  const { data: result } = await api.get(movieDiscover, {
+    params: {
+      page: 8,
+      api_key: apiKey,
+      language: ptBR,
+      include_adult: false,
+    },
+  });
+
+  const tst = result['results'].map((movie) => {
+    const dataMounted = {
+      id: movie['id'],
+      title: movie['title'],
+      description: movie['overview'],
+      genreIDs: movie['genre_ids'],
+      rating: movie['vote_average'],
+    };
+
+    if (
+      movie['poster_path'] == null ||
+      movie['poster_path'] == undefined ||
+      movie['poster_path'] == ''
+    )
+      return {
+        ...dataMounted,
+        poster: '/assets/poster-placeholder.png',
+      };
+    else
+      return {
+        ...dataMounted,
+        poster: `https://image.tmdb.org/t/p/original${movie['poster_path']}`,
+      };
+  });
+
+  return res.json({
+    ok: true,
+    api_req: 'Requisição pelo arquivo index.ts!',
+    tst,
+  });
+}
 
 export const fetchMovieDiscover = async (): Promise<
   FetchMovieDiscoverProps[]
