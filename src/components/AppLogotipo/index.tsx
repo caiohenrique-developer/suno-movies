7bc7cca7e91d24364099f8ca17390d3a122fb69b
caiohenrique-developer <caiorussellrox@msn.com>
2021-08-12 18:04:28 -0300

    :recycle: [ REFACTOR ]: Import React package and type the AppLogotipo component return

--- src/components/AppLogotipo/index.tsx
+++ src/components/AppLogotipo/index.tsx
@@ -1,5 +1,7 @@
+import React from 'react';
+
 import LogotipoSvg from '@assets/app-logotipo.svg';
 
-export const AppLogotipo = () => {
+export const AppLogotipo = (): JSX.Element => {
   return <LogotipoSvg />;
 };
