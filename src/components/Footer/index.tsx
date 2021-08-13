45b857ff1f5e0eb8fd7a6533c3836f8583ac014c
caiohenrique-developer <caiorussellrox@msn.com>
2021-08-13 18:48:01 -0300

    :sparkles: [ FEAT ]: Add repository hiperlink to Footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
+import Link from 'next/link';
+
 import { Container } from './style';
 
 export const Footer = (): JSX.Element => {
   return (
     <Container>
-      <p>© 2021 Suno Movies. All rights reserved.</p>
+      <p>
+        © 2021 Suno Movies, done with ♥ by
+        <Link href='https://github.com/caiohenrique-developer/suno-movies'>
+          <a href='dummy'>caiohenrique-developer</a>
+        </Link>
+        , all rights reserved.
+      </p>
     </Container>
   );
 };
