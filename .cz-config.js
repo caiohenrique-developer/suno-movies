a0c2594e1bd982eb6e8a7743173e85e4bf4dd6ee
caiohenrique-developer <caiorussellrox@msn.com>
2021-08-19 17:27:05 -0300

    :rotating_light: 🚀 [ TEST ]: Add emoji to prompt view

--- .cz-config.js
+++ .cz-config.js
@@ -17,7 +17,7 @@ module.exports = {
       value: ':rocket: [ PERF ]',
       name: '[ PERF ]: A code change that improves performance',
     },
-    { value: ':rotating_light: [ TEST ]', name: '[ TEST ]: Adding missing tests' },
+    { value: ':rotating_light: 🚀 [ TEST ]', name: '🛸 [ TEST ]: Adding missing tests' },
     {
       value: ':hammer: [ CHORE ]',
       name:
