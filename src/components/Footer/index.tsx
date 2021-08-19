78baff4cb2e9f0ce55253eab1fdd7b5d782758b4
caiohenrique-developer <caiorussellrox@msn.com>
2021-08-19 14:06:23 -0300

    :recycle: [ REFACTOR ]: Add date validation to footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,14 +5,19 @@ import Link from 'next/link';
 import { Container } from './style';
 
 export const Footer = (): JSX.Element => {
+  const currentYear = new Date().getFullYear();
+  const year = currentYear > 2021 ? `2021 - ${currentYear}` : currentYear;
+
   return (
     <Container>
       <p>
-        © 2021 Suno Movies, done with ♥ by
+        Copyright &copy; {year} Suno Movies, done with ☕ by{' '}
         <Link href='https://github.com/caiohenrique-developer/suno-movies'>
-          <a href='dummy'>caiohenrique-developer</a>
+          <a target='_blank' rel='noreferrer'>
+            caiohenrique-developer
+          </a>
         </Link>
-        , all rights reserved.
+        .
       </p>
     </Container>
   );
